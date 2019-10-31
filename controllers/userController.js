77e500b67bfe61a1d5eb7bcae3f77efa6c851bc9
palewing <rendo.030@gmail.com>
2019-11-01 00:09:57 +0700

    nearby cinema, register, login

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,49 @@
 const User = require('../models/user');
 const hashPassword = require('../helpers/hashPassword');
 const jwtHandler = require('../helpers/jwtHandler');
+const googleMapsClient = require('@google/maps').createClient({
+    key: 'AIzaSyCkoZnz8u6uszDXFRyMzajapv1_K9WlYAY',
+    Promise: Promise
+  });
 
 class UserController {
+    static register(req, res){
+        User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password
+        })
+        .then(user=>{
+            res.status(201).json({
+                msg:"new user successfully created",
+                data: user
+            })
+        })
+        .catch(err=>{
+            res.status(500).json(err);
+        })
+    }
+
+    static login(req, res){
+        User.findOne({
+            email: req.body.email
+        })
+        .then(user=>{
+            if(hashPassword.compare(req.body.password, user.password)){
+                let jwtToken = jwtHandler.generateJWT(user.email);
+                res.status(200).json(jwtToken);
+            }
+            else{
+                res.status(400).json({
+                    msg: 'wrong email for password'
+                })
+            }
+        })
+        .catch(err=>{
+            res.status(500).json(err);
+        })
+    }
+
     static googleLogin(req, res){
         User.findOne({
             email: req.user.email
@@ -22,6 +63,20 @@ class UserController {
         .then(user=>{
             let jwtToken = jwtHandler.generateJWT(user.email);
             res.status(200).json(jwtToken);
+        })
+        .catch(err=>{
+            res.status(500).json(err);
+        })
+    }
+
+    static getNearbyCinemas(req, res){
+        googleMapsClient.findPlace({
+            input: 'cinema xxi',
+            inputtype: 'textquery',
+            fields: ['photos','formatted_address','name','rating','opening_hours','geometry']      
+        }).asPromise()
+        .then(response=>{
+            res.status(200).json(response);
         })
         .catch(err=>{
             res.status(500).json(err);
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,11 @@
 const router = require("express").Router();
 const UserController = require('../controllers/userController');
 const googleVerify = require('../middlewares/googleVerify');
+const userRouter = require('./userRouter')
 
 router.post('/google-signin', googleVerify, UserController.googleLogin);
+router.get('/cinemas', UserController.getNearbyCinemas);
+
+router.use('/users', userRouter);
 
 module.exports = router;

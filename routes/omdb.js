4e5a14ad4a2560500441383d86462b9e55e54026
Edison <edirates@gmail.com>
2019-11-01 13:07:27 +0700

    Revising api for imdb

--- routes/omdb.js
+++ routes/omdb.js
@@ -3,6 +3,6 @@ const OmdbController = require("../controllers/omdb.js");
 const authenticate = require("../middlewares/authenticate.js");
 
 // router.use(authenticate);
-router.get("/search", OmdbController.search);
+router.post("/search", OmdbController.search);
 
 module.exports = router;
